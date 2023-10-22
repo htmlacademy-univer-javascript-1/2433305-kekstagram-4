ee2d36cb88ba2e4125100ab26e2af5df2f3d9c55
Keks <keks@htmlacademy.ru>
2023-10-22 15:35:11 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,4 @@
+import { getPhotoArray } from './getPhotoArray.js';
+
+const photos = getPhotoArray();
+console.log(photos)
---
+++ 8/js/utils.js
@@ -0,0 +1,8 @@
+export const getRandomInt = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+export const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
