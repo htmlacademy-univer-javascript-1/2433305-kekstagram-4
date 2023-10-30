c7a3abf26767743b3f86fccad7b13fda7858afe4
Сергей Михайлов <mihailovserant2004@mail.ru>
2023-10-30 15:16:52 +0500

    Внес в один входной файл main.js

--- js/bigPhotoModal.js
+++ js/bigPhotoModal.js
@@ -1,13 +1,40 @@
 const commentTemplate = document.querySelector('.social__comment');
 
+const downloadMoreButton = document.querySelector('.social__comments-loader');
+
+const commentsLoader = document.querySelector('.comments-loader');
+
+
 const onDocumentKeydown = (evt) => {
   if (evt.key === 'Escape') {
     closePicture(evt);
   }
 };
 
+downloadMoreButton.addEventListener('click', function(evt){
+  download5Photos();
+});
+
+const download5Photos =() =>{
+  let currentComment = document.querySelector('.social__comment.hidden');
+  let i = 0;
+  for (; i < 5; i++) {
+    if (currentComment === null) {
+      commentsLoader.classList.add('hidden');
+      break;
+    }
+    currentComment.classList.remove('hidden');
+    currentComment = currentComment.nextElementSibling;
+  }
+  document.querySelector('.active__comments-count').textContent = (+document.querySelector('.active__comments-count').textContent) + i;
+};
+
+
 function openPicture(evt, url, description, likes, comments) {
+  document.querySelector('.active__comments-count').textContent = 5;
+  if (comments.length == 0){ document.querySelector('.active__comments-count').textContent = 0;}
   const openedPicture = document.querySelector('.big-picture');
+  commentsLoader.classList.remove('hidden');
   openedPicture.classList.remove('hidden');
   openedPicture.querySelector('.big-picture__img img').src = url;
   openedPicture.querySelector('.likes-count').textContent = likes;
@@ -15,21 +42,40 @@ function openPicture(evt, url, description, likes, comments) {
   openedPicture.querySelector('.social__caption').textContent = description;
 
   const commentsFragment = document.createDocumentFragment();
-  for (let i = 0; i < comments.length; ++i) {
-    const comment = commentTemplate.cloneNode(true);
-    comment.querySelector('.social__picture').src = comments[i].avatar;
-    comment.querySelector('.social__picture').alt = comments[i].name;
-    comment.querySelector('.social__text').textContent = comments[i].message;
-    commentsFragment.append(comment);
-  }
+  if(comments.length >= 5){
+    for (let i = 0; i < 5; ++i) {
+      const comment = commentTemplate.cloneNode(true);
+      comment.querySelector('.social__picture').src = comments[i].avatar;
+      comment.querySelector('.social__picture').alt = comments[i].name;
+      comment.querySelector('.social__text').textContent = comments[i].message;
+      commentsFragment.append(comment);
+    }
 
+    for (let i = 5; i < comments.length; ++i) {
+      const comment = commentTemplate.cloneNode(true);
+      comment.querySelector('.social__picture').src = comments[i].avatar;
+      comment.querySelector('.social__picture').alt = comments[i].name;
+      comment.querySelector('.social__text').textContent = comments[i].message;
+      comment.classList.add('hidden')
+      commentsFragment.append(comment);
+    }
+  }
+  else{
+    for (let i = 0; i < comments.length; i++){
+      document.querySelector('.active__comments-count').textContent = comments.length;
+      commentsLoader.classList.add('hidden');
+      const comment = commentTemplate.cloneNode(true);
+      comment.querySelector('.social__picture').src = comments[i].avatar;
+      comment.querySelector('.social__picture').alt = comments[i].name;
+      comment.querySelector('.social__text').textContent = comments[i].message;
+      commentsFragment.append(comment);
+    }
+  }
 
   const commentsContainer = document.querySelector('.social__comments');
   commentsContainer.innerHTML = '';
   commentsContainer.append(commentsFragment);
 
-  document.querySelector('.social__comment-count').classList.add('hidden');
-  document.querySelector('.comments-loader').classList.add('hidden');
   document.body.classList.add('modal-open');
 
   openedPicture.querySelector('.big-picture__cancel').addEventListener('click', closePicture);
