36372bba46cbd31c4a294485a4937876622de462
Keks <keks@htmlacademy.ru>
2023-10-30 18:48:44 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,5 @@
+import { getPhotoArray } from './getPhotoArray.js';
+import { getRenderedCards } from './drawMiniature.js';
+const photos = getPhotoArray();
+console.log(photos)
+getRenderedCards();
