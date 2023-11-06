279f53f721a739ec4ad422848bd7b91ecee99da6
Сергей Михайлов <mihailovserant2004@mail.ru>
2023-11-06 13:56:30 +0500

    Не понимаю как отобразить слайдер, помогите пожалуйста

--- js/downloadNewPost.js
+++ js/downloadNewPost.js
@@ -1,4 +1,4 @@
-import { onDocumentKeydown } from "./utils.js";
+import { onDocumentKeydown } from './utils.js';
 
 const uploadForm = document.querySelector('.img-upload__form');
 const uploadInput = uploadForm.querySelector('.img-upload__input');
@@ -6,7 +6,15 @@ const imgOverlay = uploadForm.querySelector('.img-upload__overlay');
 const closeButton = uploadForm.querySelector('.img-upload__cancel');
 const hashtagsField = uploadForm.querySelector('.text__hashtags');
 const descriptionField = uploadForm.querySelector('.text__description');
+const scaleSmaller = uploadForm.querySelector('.scale__control--smaller');
+const scaleBigger = uploadForm.querySelector('.scale__control--bigger');
+const scaleValue = uploadForm.querySelector('.scale__control--value');
+const previewPicture = uploadForm.querySelector('.img-upload__preview img');
+const slider = uploadForm.querySelector('.effect-level__slider');
+const effectLevelValue = uploadForm.querySelector('.effect-level__value');
+const sliderContainer = uploadForm.querySelector('.img-upload__effect-level');
 
+//Validation of fieldsets
 const regExp = /^#[0-9a-zа-яё]{1,19}$/i;
 
 const pristine = new Pristine(uploadForm, {
@@ -21,6 +29,7 @@ const pristine = new Pristine(uploadForm, {
 const validateHashtagsCount = (value) => {
   return value.trim().split(' ').length <= 5;
 }
+
 
 const validateHashtagsUniqueness = (value) => {
   return value.trim().split(' ').includes(value.trim()) == false;
@@ -66,7 +75,10 @@ pristine.addValidator(
   validateDescription,
   'Длина описания не может быть больше 140 символов'
 );
+//End of validation of fieldsets
 
+
+//Overlay controllers
 const hideOverlay = () => {
   imgOverlay.classList.add('hidden');
   document.removeEventListener('keydown', onDocumentKeydown(closeOverlay));
@@ -83,6 +95,7 @@ const openOverlay = (evt) =>{
   closeButton.addEventListener('click', closeOverlay);
   document.addEventListener('keydown', onDocumentKeydown(closeOverlay));
   uploadInput.removeEventListener('click', openOverlay);
+  setDefaultScale();
 }
 
 const closeOverlay = (evt) => {
@@ -97,3 +110,144 @@ const closeOverlay = (evt) => {
 }
 
 uploadInput.addEventListener('change', openOverlay);
+//End of overlay controllers
+
+//Scaling controllers
+const setDefaultScale = () => {
+  scaleValue.value = '100%';
+  previewPicture.style.transform = 'scale(1)';
+}
+
+const onScaleBigger = (evt) => {
+  const currentValue = parseInt(scaleValue.value);
+  scaleValue.value = currentValue <= 75 ? `${currentValue + 25}%` : `${currentValue}%`;
+  previewPicture.style.transform = `scale(${parseInt(scaleValue.value) * 0.01})`;
+}
+
+const onScaleSmaller = (evt) => {
+  const currentValue = parseInt(scaleValue.value)
+  scaleValue.value = currentValue >= 50 ? `${currentValue - 25}%` : `${currentValue}%`;
+  previewPicture.style.transform = `scale(${parseInt(scaleValue.value) * 0.01})`
+}
+
+scaleBigger.addEventListener('click', onScaleBigger);
+scaleSmaller.addEventListener('click', onScaleSmaller);
+//End of scaling controllers
+
+//Filters controllers
+noUiSlider.create(slider, {
+  range: {
+    min: 0,
+    max: 1
+  },
+  start: 0,
+  step: 1,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value;
+      }
+      return value.toFixed(1);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
+});
+
+function setDefaultFilter() {
+  previewPicture.style.filter = 'none';
+  sliderContainer.style.display = 'none';
+}
+
+function onFilterClick(evt) {
+  const effect = evt.target.value;
+  setDefaultFilter();
+  if (effect !== 'none') {
+    sliderContainer.style.display = 'block';
+  }
+  switch (effect) {
+    case 'chrome':
+      slider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 1,
+        },
+        start: 1,
+        step: 0.1
+      });
+      slider.noUiSlider.set(1);
+      break;
+    case 'sepia':
+      slider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 1,
+        },
+        start: 1,
+        step: 0.1
+      });
+      slider.noUiSlider.set(1);
+      break;
+    case 'marvin':
+      slider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 100,
+        },
+        start: 100,
+        step: 1
+      });
+      slider.noUiSlider.set(100);
+      break;
+    case 'phobos':
+      slider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 3,
+        },
+        start: 3,
+        step: 0.1
+      });
+      slider.noUiSlider.set(3);
+      break;
+    case 'heat':
+      slider.noUiSlider.updateOptions({
+        range: {
+          min: 1,
+          max: 3,
+        },
+        start: 3,
+        step: 0.1
+      });
+      slider.noUiSlider.set(3);
+      break;
+  }
+}
+
+slider.noUiSlider.on('update', () => {
+  effectLevelValue.value = slider.noUiSlider.get();
+  const effect = uploadForm.querySelector('.effects__radio:checked').value;
+  switch (effect) {
+    case 'chrome':
+      previewPicture.style.filter = `grayscale(${effectLevelValue.value})`;
+      break;
+    case 'sepia':
+      previewPicture.style.filter = `sepia(${effectLevelValue.value})`;
+      break;
+    case 'marvin':
+      previewPicture.style.filter = `invert(${effectLevelValue.value}%)`;
+      break;
+    case 'phobos':
+      previewPicture.style.filter = `blur(${effectLevelValue.value}px)`;
+      break;
+    case 'heat':
+      previewPicture.style.filter = `brightness(${effectLevelValue.value})`;
+      break;
+  }
+});
+
+document.querySelectorAll('.effects__radio').forEach((li) => {
+  li.addEventListener('click', onFilterClick);
+});
