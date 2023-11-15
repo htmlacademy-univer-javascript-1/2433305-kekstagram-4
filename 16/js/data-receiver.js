53a545b353fa4e7f84e72a19c7a57af4e4088a0d
Keks <keks@htmlacademy.ru>
2023-11-15 20:05:20 +0300

    :heavy_check_mark: Сборка #16

---
+++ 16/js/data-receiver.js
@@ -0,0 +1,42 @@
+import { getData } from './data-loader.js';
+import { renderPictures } from './drawMiniature.js';
+import { showFilter, setFilterClick} from './filter.js';
+import { debounce } from './utils.js';
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, 5000);
+};
+
+const setDataFromServer = () => {
+  getData()
+    .then((pictures) => {
+      showFilter();
+      renderPictures(pictures);
+      setFilterClick(debounce(
+        () => renderPictures(pictures),
+        500,
+      ));
+    })
+    .catch((error) => {
+      showAlert(error.message);
+    });
+};
+
+export {setDataFromServer};
---
+++ 16/js/utils.js
@@ -0,0 +1,91 @@
+//import {commentMessages, commentNames} from './data.js';
+
+function getRandomInteger (min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      previousValues.length = 0;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function shuffle(arr){
+  let j, temp;
+  for(let i = arr.length - 1; i > 0; i--){
+    j = Math.floor(Math.random()*(i + 1));
+    temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
+
+export {getRandomInteger, createRandomIdFromRangeGenerator, debounce, shuffle};
+/*
+function getRandomCommentMessage() {
+  let output = '';
+  for (let i = 0; i < getRandomInteger(1, 2); ++i) {
+    if (i !== 0) {
+      output += ' ';
+    }
+    output += commentMessages[getRandomInteger(0, commentMessages.length - 1)];
+  }
+  return output;
+}
+
+function getRandomCommentName() {
+  return commentNames[getRandomInteger(0, commentNames.length - 1)];
+}
+
+let currentId = 1;
+
+const generateCommentId = createRandomIdFromRangeGenerator(0, 1000);
+
+function createComment() {
+  return {
+    id: generateCommentId(),
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: getRandomCommentMessage(),
+    name: getRandomCommentName(),
+  };
+}
+
+function createPost() {
+  return {
+    id: currentId,
+    url: `photos/${currentId++}.jpg`,
+    description: 'ОПИСАНИЕ',
+    likes: getRandomInteger(15, 200),
+    comments: Array.from({length: getRandomInteger(0, 30)}, createComment),
+  };
+}
+
+function createPosts(n) {
+  return Array.from({length: n}, createPost);
+}
+*/
